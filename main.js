b324456fcc88b942a65e0f42c671edfd4f66a59c
mathew <mbush11@vols.utk.edu>
2021-01-25 12:35:28 -0500

    debugged beat and timing

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 window.addEventListener('load', e =>{
     var strike = document.querySelector('#strike')
+    var start = document.querySelector('#start')
+    var pause = document.querySelector('#pause')
+    var t_e = {}
     var intID 
-    var t = new Date()
-    var m = new Math()
+    
 
     // plays sound
     function audio_play(name){
@@ -16,30 +18,48 @@ window.addEventListener('load', e =>{
         var cnt = 0
 
         intID = setInterval(function(){
+            
             if(cnt > subdivison){
-                console.log(t.getTime())
+                tt = new Date()
+                t_e = tt.getTime()
             }
+            cnt += 1
         },delay)
     }
 
 
     // evaluates timing accuracy
-    function timing(t_e){
+    function timing(t_e,tempo){
+        var t = new Date()
         var t_o = t.getTime()
-        var delta = t_e - t_o
         
-        if(delta < 1){                      // rushing
-            var score = -m.exp(delta/1000)
+        
+        if(t_o > t_e + 0.5*6E4/tempo){    
+            var delta = t_e + 6E4/tempo - t_o 
+            var score = -Math.exp(delta/1000)
         }
-        else{                              // dragging
-            var score = m.exp(delta/1000)
+        else{          
+            var delta = t_o - t_e      
+            var score = Math.exp(delta/1000)
         }
-        return score
+
+        console.log(score)
     }
     
     
     strike.addEventListener('click',function(){
         audio_play('strike')
+        timing(t_e,120)
         
     })
+
+    start.addEventListener('click', e => {
+        beat(120,4)
+    })
+
+    pause.addEventListener('click', function(){
+        clearInterval(intID)
+    })
+
+    
 })
