a934dd7f0ce8552963982c54e7b77a43035bd862
mathew <mbush11@vols.utk.edu>
2021-01-24 20:44:02 -0500

    added beat function

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 window.addEventListener('load', e =>{
     var strike = document.querySelector('#strike')
-    
+    var intID 
+    var t = new Date()
+    var m = new Math()
+
     // plays sound
     function audio_play(name){
         var audio = new Audio('./sounds/' + name + '.mp3')
         audio.play();
     }
 
+    // logs the beat
+    function beat(tempo,subdivison){
+        delay = 60000/tempo
+        var cnt = 0
+
+        intID = setInterval(function(){
+            if(cnt > subdivison){
+                console.log(t.getTime())
+            }
+        },delay)
+    }
+
+
+    // evaluates timing accuracy
     function timing(t_e){
-        var t = new Date()
-        var m = new Math()
         var t_o = t.getTime()
         var delta = t_e - t_o
         
